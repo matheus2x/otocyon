a8add0e89ff21978eed3e73e8b2e3dcb23c8df15
matheus2x <matheushenriqueplus99@gmail.com>
2022-04-14 16:46:41 -0300

    deploy test 3

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -6,5 +6,5 @@ const dotenvFile = path.resolve(dirName, "..", "..", "..", ".env");
 
 dotenv.config({ path: dotenvFile });
 
-export const nodePort = process.env.NODE_PORT || 3333;
+export const nodePort = process.env.PORT || 3333;
 export const steamKey = process.env?.STEAM_KEY;
